0c791fb046641ecf14c07296859defb6e03106c1
Josh Pollock <jopo@mit.edu>
2022-12-05 13:57:38 -0500

    ensure binding works for dynamic

--- src/test/dynamic-test.svelte
+++ src/test/dynamic-test.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+  import Display from "../components/Display.svelte";
   import Dynamic from "../components/Dynamic.svelte";
 
   let myVar = 10;
 </script>
 
-<Dynamic value={myVar} />
+<Dynamic bind:value={myVar} />
+
+value: <Display value={myVar} />
